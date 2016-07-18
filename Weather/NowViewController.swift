656410b8bfc71a8489d825ac7001a3f13dbdbd91
Asmins <admin@MacBook-Air-Asmins.local>
2016-07-18 14:41:03 +0300

    Start project(pageView)

---
+++ Weather/NowViewController.swift
@@ -0,0 +1,54 @@
+//
+//  NowViewController.swift
+//  Weather
+//
+//  Created by admin on 18.07.16.
+//  Copyright © 2016 Mozi. All rights reserved.
+//
+
+import UIKit
+
+class NowViewController: UIViewController {
+
+    @IBOutlet weak var labelForTypeWeather: UILabel!
+    
+    @IBOutlet weak var labelForTemperature: UILabel!
+    
+    @IBOutlet weak var labelForHumidity: UILabel!
+    
+    @IBOutlet weak var labelForUvIndex: UILabel!
+    
+    @IBOutlet weak var labelForWindSpeed: UILabel!
+    
+    @IBOutlet weak var imageForTypeWeather: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func moreDetail(sender: AnyObject) {
+        
+        
+    }
+    @IBAction func showHourlyView(sender: AnyObject) {
+    
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Weather/PageViewController.swift
@@ -0,0 +1,87 @@
+//
+//  PageViewController.swift
+//  Weather
+//
+//  Created by admin on 18.07.16.
+//  Copyright © 2016 Mozi. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController,UIPageViewControllerDataSource {
+
+    
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newColoredViewController("Now"),self.newColoredViewController("Hourly"),self.newColoredViewController("Daily")]
+    }()
+    
+    
+    func newColoredViewController(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewControllerWithIdentifier("\(name)ViewController")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],direction: .Forward,animated: true,completion: nil)
+        }
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        // User is on the first view controller and swiped left to loop to
+        // the last view controller.
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        // User is on the last view controller and swiped right to loop to
+        // the first view controller.
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
