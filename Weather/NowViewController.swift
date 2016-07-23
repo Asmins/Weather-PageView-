c5707ab2903c0c27aa31ecc6da33c9de4b338d41
Asmins <admin@MacBook-Air-Asmins.local>
2016-07-23 16:15:56 +0300

    Setting NowView

--- Weather/NowViewController.swift
+++ Weather/NowViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class NowViewController: UIViewController {
 
     @IBOutlet weak var labelForTypeWeather: UILabel!
+    @IBOutlet weak var labelForNextDayTypeWeather: UILabel!
     
     @IBOutlet weak var labelForTemperature: UILabel!
     
+    @IBOutlet weak var labelForNextDayForTemperature: UILabel!
     @IBOutlet weak var labelForHumidity: UILabel!
     
     @IBOutlet weak var labelForUvIndex: UILabel!
@@ -25,23 +27,27 @@ class NowViewController: UIViewController {
     @IBOutlet weak var activitityInd: UIActivityIndicatorView!
     
     var weatherDaily = [WeatherDaily]()
-    
+    var uvIndex = 0.0
                                                                             //(lat),(lon)
     let urlDaily = "http://api.wunderground.com/api/4ed7dad052717db4/forecast10day/q/34,-118.json"
-    
+    let urlUvIndex = "http://api.owm.io/air/1.0/uvi/current?lat=34&lon=-118&appid=fe96847f962cbea42c4d879c33daf010"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getDataAboutWeather(urlDaily)
-        
+        getDataAboutWeather(urlUvIndex)
         let alert = UIAlertController(title: "Alert", message: "Please wait 5 or 10 seconds for load data weather", preferredStyle: UIAlertControllerStyle.Alert)
         
         alert.addAction(UIAlertAction(title: "Enter", style: UIAlertActionStyle.Default, handler: {action in
             switch action.style{
             case .Default:
-                print(self.weatherDaily[0].humidity)
-                print("Some Action")
+                self.labelForWindSpeed.text = "\(self.weatherDaily[0].wind_speed) KM/H"
+                self.labelForHumidity.text = "\(self.weatherDaily[0].humidity)%"
+                self.labelForTypeWeather.text = self.weatherDaily[0].typeWeatherForDaily
+                self.labelForTemperature.text = "\(self.weatherDaily[0].avarageTemperature)°"
+                self.labelForNextDayTypeWeather.text = self.weatherDaily[1].typeWeatherForDaily
+                self.labelForNextDayForTemperature.text = "\(self.weatherDaily[1].avarageTemperature)°"
             default:
                 print("Error")
         }
@@ -73,6 +79,7 @@ class NowViewController: UIViewController {
         
         task.resume()
     }
+    
     func parseJsonDataForWeatherDaily(data:NSData) -> [WeatherDaily ]{
         do{
             let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
@@ -98,7 +105,23 @@ class NowViewController: UIViewController {
                         }
                     }
                 }
-            }
+            }else if let valueUvIndex = jsonResult!["value"] as? Double{
+                switch valueUvIndex {
+                case 0..<2.9 :
+                    labelForUvIndex.text = "Low"
+                case 3..<5.9 :
+                    labelForUvIndex.text = "Modarate"
+                case 6..<7.9 :
+                    labelForUvIndex.text = "High"
+                case 8..<10.9 :
+                    labelForUvIndex.text = "Very High"
+                case 11..<100 :
+                    labelForUvIndex.text = "Extreme"
+                default:
+                    print("Error")
+                }
+                
+                }
         }catch{
             print(error)
         }
