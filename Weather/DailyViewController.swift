49d236ab9443cf2b2fefbdebbd31343a5230e88c
Asmins <admin@MacBook-Air-Asmins.local>
2016-07-24 19:00:29 +0300

    Data for dailyTableView

--- Weather/DailyViewController.swift
+++ Weather/DailyViewController.swift
@@ -8,28 +8,91 @@
 
 import UIKit
 
-class DailyViewController: UIViewController {
+class DailyViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var url = "http://api.wunderground.com/api/4ed7dad052717db4/forecast10day/q/34,-118.json"
+    
+    var weather = [WeatherDaily]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getWeather()
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weather.count
+    }
+
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! CustomTableViewCellForDaily
+        cell.labelForDate.text = "\(weather[indexPath.row].day)/\(weather[indexPath.row].month)"
+        cell.labelForTypeWeather.text = "\(weather[indexPath.row].typeWeatherForDaily)"
+        cell.labelForHighLowTemperature.text = "\(weather[indexPath.row].highTemperature)°/\(weather[indexPath.row].lowTemperature)°"
+        return cell
+    }
+    
+    func getWeather(){
+        let reques = NSURLRequest(URL: NSURL(string: url)!)
+        let urlSesion = NSURLSession.sharedSession()
+        let task = urlSesion.dataTaskWithRequest(reques,completionHandler: {(data,response,error)->Void in
+            if let error = error {
+                print(error)
+                return
+            }
+            if let data = data {
+                self.weather = self.parseJsonData(data)
+                
+                NSOperationQueue.mainQueue().addOperationWithBlock({()-> Void in
+                    self.tableView.reloadData()
+                })
+                
+            }
+        })
+        task.resume()
+    }
+    
+    func parseJsonData(data:NSData) -> [WeatherDaily ]{
+        do{
+            let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
+            if let root = jsonResult?["forecast"] as? [String:AnyObject] {
+                if let simpleforecastItem = root["simpleforecast"] as? [String:AnyObject]{
+                    if let forecastday = simpleforecastItem["forecastday"] as? [[String:AnyObject]] {
+                        
+                        for data in forecastday {
+                            let dataAboutWeather = WeatherDaily()
+                            
+                            if let date = data["date"] as? [String:AnyObject]{
+                                dataAboutWeather.day = date["day"] as! Int
+                                dataAboutWeather.month = date["month"] as! Int
+                            }
+                            
+                            if let high = data["high"] as? [String:AnyObject]{
+                                dataAboutWeather.highTemperature = high["celsius"] as! String
+                            }
+                            if let low  = data["low"] as? [String:AnyObject]{
+                                dataAboutWeather.lowTemperature = low["celsius"] as! String
+                            }
+                            if let windSpeed = data["avewind"] as? [String:AnyObject]{
+                                dataAboutWeather.wind_speed = windSpeed["kph"] as! Int
+                            }
+                            dataAboutWeather.typeWeatherForDaily = data["conditions"] as! String
+                            dataAboutWeather.humidity = data["avehumidity"] as! Int
+                            weather.append(dataAboutWeather)
+                        }
+                    }
+                }
+            }
+        }catch{
+                print("Error")
+            }
+            return weather
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- Weather/MoreDetailViewController.swift
+++ Weather/MoreDetailViewController.swift
@@ -12,18 +12,14 @@ class MoreDetailViewController: UIViewController {
 
     @IBAction func backButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(false, completion: nil)
-        print("Fuck")
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     
     override func prefersStatusBarHidden() -> Bool {
         return true
